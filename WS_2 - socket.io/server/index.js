7d4ff1333974d6566d8c744629a8179c68c2ccc0
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-09 14:40:04 +0530

    setup the backend

--- WS_2 - socket.io/server/index.js
+++ WS_2 - socket.io/server/index.js
@@ -1,13 +1,22 @@
-const ws = require("ws");
-const server = new ws.Server({ port: 8080 });
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-server.on("connection", (socket) => {
-  console.log("Client connected");
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected ===> ", socket.id);
 
   socket.on("message", (message) => {
-    const b = Buffer.from(message, "base64");
-    console.log(b.toString());
-
-    socket.send(`${message} from server`);
+    console.log(message);
+    io.emit("message", `${message} from server & ${socket.id}`);
   });
+});
+
+httpServer.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
