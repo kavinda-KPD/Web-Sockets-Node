e9a3a82fc2fa78c854af0fbdff8c061f97fc5e25
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-11 10:29:30 +0530

    implement backend to listen and emit messages

--- WS_4 - activity-detection/server/index.js
+++ WS_4 - activity-detection/server/index.js
@@ -23,8 +23,32 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
   console.log("Client connected ===> ", socket.id);
 
+  // upon connection only to user
+  socket.emit("message", `${socket.id} welcome to the server`);
+
+  //upon connection to all users
+  socket.broadcast.emit("message", `${socket.id} joined the server`);
+
+  //Listen for messages coming from the client
   socket.on("message", (message) => {
-    console.log(message);
+    //send to all the users in the room including the sender
     io.emit("message", `${message} from server & ${socket.id}`);
+
+    //send to all the users in the room excluding the sender
+    socket.broadcast.emit(
+      "message",
+      `===> ${message} from server & ${socket.id}`
+    );
+  });
+
+  // upon disconnection
+  socket.on("disconnect", () => {
+    //send to all the users in the room excluding the sender
+    socket.broadcast.emit("message", `${socket.id} left the server`);
+  });
+
+  // listen for activity coming from the client and broadcast to all
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
   });
 });
