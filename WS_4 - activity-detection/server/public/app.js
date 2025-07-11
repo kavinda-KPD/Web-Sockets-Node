9b994f32480c66d6e0ded739fde9cd448bf4be92
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-11 10:43:15 +0530

    Implement frontend to catch and display the activity event

--- WS_4 - activity-detection/server/public/app.js
+++ WS_4 - activity-detection/server/public/app.js
@@ -16,11 +16,27 @@ document.querySelector("form").addEventListener("submit", sendMessage);
 
 //listen for messages coming from the server
 socket.on("message", (data) => {
+  activity.textContent = "";
   const li = document.createElement("li");
   li.textContent = data;
   document.querySelector("ul").appendChild(li);
 });
 
+//send typing activity to the server
 msgInput.addEventListener("keypress", () => {
   socket.emit("activity", `${socket.id} is typing...`);
+});
+
+let activityTimer;
+
+//listen for typing activity coming from the server
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  //clear the timer after 2 seconds
+  clearTimeout(activityTimer);
+
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 2000);
 });
